cbfa976285a7e8aa7782d2c12b0febb30c71b03a
David Cortes <dcortes22@gmail.com>
2017-05-13 15:27:09 -0600

    Fix migration

--- db/migrate/20170513010612_create_users.rb
+++ db/migrate/20170513010612_create_users.rb
@@ -3,6 +3,7 @@ class CreateUsers < ActiveRecord::Migration[5.0]
     create_table :users do |t|
       t.string :name
       t.string :last_name
+      t.string :email,              null: false, default: ""
 
       t.timestamps
     end
