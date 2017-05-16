f1f10a57fcb184eb049a621a8223189605988a75
David Cortes <dcortes22@gmail.com>
2017-05-16 14:01:35 -0600

    Fix deploy sample

--- config/deploy.rb
+++ config/deploy.rb
@@ -47,14 +47,24 @@ namespace :deploy do
   end
 
   task :seed => [:set_rails_env] do
-  on primary fetch(:migration_role) do
-    within release_path do
-      with rails_env: fetch(:rails_env) do
-        execute :rake, "db:seed"
+    on primary fetch(:migration_role) do
+      within release_path do
+        with rails_env: fetch(:rails_env) do
+          execute :rake, "db:seed"
+        end
       end
     end
   end
-end
+
+  task :token => [:set_rails_env] do
+    on primary fetch(:migration_role) do
+      within release_path do
+        with rails_env: fetch(:rails_env) do
+          execute :rake, "token:add"
+        end
+      end
+    end
+  end
 
   after :publishing, :restart
 
