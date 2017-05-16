183b7b29349261230593e75665935732718a3782
David Cortes <dcortes22@gmail.com>
2017-05-16 13:54:47 -0600

    Fix deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -54,15 +54,7 @@ namespace :deploy do
       end
     end
   end
-
-  task :token => [:set_rails_env] do
-  on primary fetch(:migration_role) do
-    within release_path do
-      with rails_env: fetch(:rails_env) do
-        execute :rake, "token:add"
-      end
-    end
-  end
+end
 
   after :publishing, :restart
 
