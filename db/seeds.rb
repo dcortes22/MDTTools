07023c31282150772f5e7ced6bbc6c9e7607bfcd
David Cortes <dcortes22@gmail.com>
2017-10-06 14:53:31 -0600

    Edit seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,12 @@
-role1 = Role.find_or_initialize_by(name: 'Admin')
-role2 = Role.find_or_initialize_by(name: 'Sales')
-role3 = Role.find_or_initialize_by(name: 'Integrator')
+# role1 = Role.find_or_initialize_by(name: 'Admin')
+# role2 = Role.find_or_initialize_by(name: 'Sales')
+# role3 = Role.find_or_initialize_by(name: 'Integrator')
+#
+# role1.save!
+# role2.save!
+# role3.save!
 
-role1.save!
-role2.save!
-role3.save!
 
-user = User.first
+user = User.create(name:"David", last_name:"Cortes", email:"dcortess@akamai.com", password:"david123")
 user.role_id = 1
 user.save!
